d9e45b02fc2703de88bb93b31d11a529d9b6b5e4
riccardo <rik.todaro@gmail.com>
2024-08-05 17:24:36 +0200

    ok

---
+++ libft_prova/ft_memset.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+void    *ft_memset(void *s, int c, size_t n)
+{
+    size_t  i;
+    unsigned char   *buffer = (unsigned char *)s;
+    i = 0;
+    while (i < n)
+    {
+        buffer[i] = (unsigned char)c;
+        i++;
+    }
+    return (buffer);
+}
